3c5d17a991502717c905d7d4477ce5276103d193
Logan <monday.hopscotch@gmail.com>
2023-08-14 20:50:39 -0600

    ability demos, credit update, map cleanup

--- source/entities/DummyPlayer.hx
+++ source/entities/DummyPlayer.hx
@@ -1,5 +1,6 @@
 package entities;
 
+import collision.ColorCollideSprite;
 import flixel.effects.particles.FlxEmitter;
 import entities.particles.DeathParticles;
 import loaders.Aseprite;
@@ -8,23 +9,79 @@ import flixel.FlxSprite;
 
 using bitdecay.flixel.extensions.FlxObjectExt;
 
-class DummyPlayer extends FlxSprite {
+class DummyPlayer extends ColorCollideSprite {
 	private static var anims = AsepriteMacros.tagNames("assets/aseprite/characters/player.json");
 
 	public var emitterCB:Array<FlxEmitter>->Void = null;
 
 	public function new(centerX:Float, centerY:Float) {
-		super(centerX, centerY);
+		super(centerX, centerY, EMPTY);
 
 		Aseprite.loadAllAnimations(this, AssetPaths.player__json);
 		animation.play(anims.powerup);
 		animation.finishCallback = finished;
 
+		animation.callback = (name, frameNumber, frameIndex) -> {
+			if (name == anims.run) {
+				if (frameNumber == 4 || frameNumber == 10)  {
+					FmodManager.PlaySoundOneShot(FmodSFX.PlayerStep);
+				}
+			}
+		}
+
 		this.setPositionMidpoint(centerX, centerY);
+	}
+
+	public function jump() {
+		animation.play(anims.jump);
+		FmodManager.PlaySoundOneShot(FmodSFX.PlayerJump4);
+	}
+
+	public function fall() {
+		animation.play(anims.fall);
+	}
+	
+	public function land() {
+		animation.play(anims.stand);
+		FmodManager.PlaySoundOneShot(FmodSFX.PlayerLand1);
+	}
+
+	public function crouch() {
+		animation.play(anims.crouch);
+		FmodManager.PlaySoundOneShot(FmodSFX.PlayerDuck2);
+	}
+
+	public function uncrouch() {
+		animation.play(anims.stand);
+		FmodManager.PlaySoundOneShot(FmodSFX.PlayerUnduck2);
+	}
+
+	public function jumpCrouch() {
+		animation.play(anims.jumpCrouch);
+		FmodManager.PlaySoundOneShot(FmodSFX.PlayerDuck2);
+	}
+
+	public function unJumpCrouch() {
+		animation.play(anims.fall);
+		FmodManager.PlaySoundOneShot(FmodSFX.PlayerUnduck2);
 	}
 
 	public function explode() {
 		animation.play(anims.death);
+	}
+
+	public function run() {
+		animation.play(anims.run);
+		animation.getByName(anims.run).frameRate = 30;
+	}
+
+	public function stopRun() {
+		FmodManager.PlaySoundOneShot(FmodSFX.PlayerSkidShort);
+		animation.play(anims.skid);
+	}
+
+	public function stand() {
+		animation.play(anims.stand);
 	}
 
 	function finished(name:String) {
